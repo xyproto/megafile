ce9646597e74ab42f89a194a5f02375f43bca09d
Alexander F. Rødseth <rodseth@gmail.com>
2025-12-03 13:06:08 +0100

    Fix the text writing

--- main.go
+++ main.go
@@ -11,30 +11,44 @@ import (
 	"time"
 
 	"github.com/xyproto/env/v2"
+	"github.com/xyproto/files"
 	"github.com/xyproto/vt"
 )
 
 type State struct {
-	dir1          string
 	c             *vt.Canvas
+	dir1          string
+	dir2          string
+	dir3          string
 	quit          bool
 	startx        uint
 	starty        uint
 	prompt_length uint
-	written       string
+	written       []rune
 }
 
 const (
 	start_message  = "---=[ MegaCLI ]=---"
 	ctrl_c_message = "bye (ctrl-c pressed)"
 	exit_message   = "bye"
+
+	leftArrow  = "←"
+	rightArrow = "→"
+	upArrow    = "↑"
+	downArrow  = "↓"
+
+	pgUpKey = "⇞" // page up
+	pgDnKey = "⇟" // page down
+	homeKey = "⇱" // home
+	endKey  = "⇲" // end
 )
 
-func ulen(s string) uint {
-	return uint(len(s))
+func ulen[T string | []rune | []int | []uint](xs T) uint {
+	return uint(len(xs))
 }
 
 func (s *State) ls(dir string) {
+	const margin = 1
 	longestSoFar := uint(0)
 	entries, err := os.ReadDir(dir)
 	if err == nil { // success
@@ -42,13 +56,23 @@ func (s *State) ls(dir string) {
 		y := s.starty + 1
 		vt.SetXY(x, y)
 		for _, e := range entries {
-			if ulen(e.Name()) > longestSoFar {
-				longestSoFar = ulen(e.Name())
+			name := e.Name()
+			if ulen(name) > longestSoFar {
+				longestSoFar = ulen(name)
 			}
-			s.c.WriteString(x, y, vt.LightBlue, vt.BackgroundDefault, e.Name())
+			path := filepath.Join(dir, name)
+			if isdir(path) {
+				s.c.WriteString(x, y, vt.Blue, vt.BackgroundDefault, name)
+				s.c.WriteString(x+ulen(name), y, vt.White, vt.BackgroundDefault, "/")
+			} else if isexec(path) {
+				s.c.WriteString(x, y, vt.LightGreen, vt.BackgroundDefault, name)
+				s.c.WriteString(x+ulen(name), y, vt.White, vt.BackgroundDefault, "*")
+			} else {
+				s.c.WriteString(x, y, vt.Default, vt.BackgroundDefault, name)
+			}
 			y++
 			if y >= s.c.H() {
-				x += longestSoFar
+				x += longestSoFar + margin
 				y = s.starty
 			}
 		}
@@ -71,12 +95,17 @@ func isfile(path string) bool {
 	return false
 }
 
-func (s *State) open(path string) error {
+func isexec(path string) bool {
+	return files.IsExecutableCached(path)
+}
+
+func (s *State) edit(filename, path string) error {
 	editorPath, err := exec.LookPath(env.Str("EDITOR"))
 	if err != nil {
 		return err
 	}
-	command := exec.Command(editorPath, path)
+	command := exec.Command(editorPath, filename)
+	command.Dir = path
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 	return command.Run()
@@ -93,7 +122,7 @@ func run(executableName string) error {
 	return command.Run()
 }
 
-func (s *State) execute(cmd string) {
+func (s *State) execute(cmd, path string) {
 	if cmd == "l" || cmd == "ls" || strings.HasPrefix(cmd, "l ") {
 		rest := ""
 		if len(cmd) > 2 {
@@ -102,18 +131,18 @@ func (s *State) execute(cmd string) {
 		if rest != "" {
 			s.ls(rest)
 		} else {
-			s.ls(s.dir1)
+			s.ls(path)
 		}
 	} else if cmd == "exit" || cmd == "quit" || cmd == "q" {
 		s.quit = true
-	} else if isdir(filepath.Join(s.dir1, cmd)) { // relative path
-		s.dir1 = filepath.Join(s.dir1, cmd)
+	} else if isdir(filepath.Join(path, cmd)) { // relative path
+		s.dir1 = filepath.Join(path, cmd)
 	} else if isdir(cmd) { // absolute path
 		s.dir1 = cmd
-	} else if isfile(filepath.Join(s.dir1, cmd)) { // relative path
-		s.open(filepath.Join(s.dir1, cmd))
+	} else if isfile(filepath.Join(path, cmd)) { // relative path
+		s.edit(cmd, path)
 	} else if isfile(cmd) { // abs absolute path
-		s.open(cmd)
+		s.edit(cmd, path)
 	}
 }
 
@@ -152,8 +181,16 @@ func main() {
 	tty.SetTimeout(10 * time.Millisecond)
 
 	var (
-		s    = &State{dir1: ".", c: c, quit: false, startx: uint(7), starty: uint(6)}
 		x, y uint
+		s    = &State{
+			c:      c,
+			dir1:   ".",
+			dir2:   env.HomeDir(),
+			dir3:   "/tmp",
+			quit:   false,
+			startx: uint(5),
+			starty: uint(6),
+		}
 	)
 
 	draw_prompt := func() {
@@ -162,20 +199,18 @@ func main() {
 		s.prompt_length = ulen(prompt)
 	}
 
-	clear_and_prepare := func() {
-		c.Clear()
-		c.Write(5, 5, vt.LightGreen, vt.BackgroundDefault, start_message)
-		draw_prompt()
-		x = s.startx + s.prompt_length
-		y = s.starty
-		c.WriteRune(x, y, vt.LightMagenta, vt.BackgroundDefault, '_')
-		vt.SetXY(x, y)
-	}
+	// The rune index for the text that has been written
+	index := uint(0)
 
 	draw_written := func() {
 		x = s.startx + s.prompt_length
 		y = s.starty
-		c.WriteString(x, y, vt.LightYellow, vt.BackgroundDefault, s.written)
+		c.WriteString(x, y, vt.LightYellow, vt.BackgroundDefault, string(s.written))
+		r := rune(' ')
+		if index < ulen(s.written) {
+			r = s.written[index]
+		}
+		c.WriteRune(x+index, y, vt.Black, vt.BackgroundYellow, r)
 		vt.SetXY(x, y)
 
 	}
@@ -188,10 +223,17 @@ func main() {
 		vt.SetXY(x, y)
 	}
 
+	clear_and_prepare := func() {
+		c.Clear()
+		c.Write(5, 5, vt.LightGreen, vt.BackgroundDefault, start_message)
+		draw_prompt()
+		x = s.startx + s.prompt_length
+		y = s.starty
+		draw_written()
+	}
+
 	clear_and_prepare()
 	c.Draw()
-
-	index := uint(0)
 
 	for !s.quit {
 		key := tty.String()
@@ -199,38 +241,65 @@ func main() {
 		case "c:27", "c:17": // esc, ctrl-q
 			s.quit = true
 		case "c:13": // return
-			if s.written == "" {
-				s.written = "ls"
+			if len(s.written) == 0 {
+				s.written = []rune("ls")
 			}
-			clear_and_prepare()
 			tmpdir := s.dir1
-			s.execute(s.written)
+			s.execute(string(s.written), s.dir1)
 			if tmpdir != s.dir1 {
 				clear_and_prepare()
 			}
-			s.written = ""
+			clear_written()
+			s.written = []rune{}
+			index = 0
+			draw_written()
 		case "c:127": // backspace
 			clear_written()
-			if index > 0 {
+			if len(s.written) > 0 {
+				s.written = append(s.written[:index-1], s.written[index:]...)
 				index--
 			}
+			draw_written()
+		case "c:11": // ctrl-k
+			clear_written()
 			if len(s.written) > 0 {
-				s.written = s.written[:len(s.written)-1]
+				s.written = s.written[:index]
 			}
 			draw_written()
 		case "c:4": // ctrl-d
 			clear_written()
 			if len(s.written) > 0 {
-				s.written = s.written[:index] + s.written[index+1:]
+				s.written = append(s.written[:index], s.written[index+1:]...)
+			}
+			draw_written()
+		case "c:1", homeKey, upArrow: // ctrl-a, home, arrow up
+			clear_written()
+			index = 0
+			draw_written()
+		case "c:5", endKey, downArrow: // ctrl-e, end, arrow down
+			clear_written()
+			index = ulen(s.written) // one after the text
+			draw_written()
+		case leftArrow:
+			clear_written()
+			if index > 0 {
+				index--
+			}
+			draw_written()
+		case rightArrow:
+			clear_written()
+			if index < ulen(s.written) {
+				index++
 			}
 			draw_written()
 		case "c:9": // tab
 			clear_written()
 			if entries, err := os.ReadDir(s.dir1); err == nil { // success
 				for _, entry := range entries {
-					if strings.HasPrefix(entry.Name(), s.written) {
-						rest := entry.Name()[len(s.written):]
-						s.written += rest
+					if strings.HasPrefix(entry.Name(), string(s.written)) {
+						rest := []rune(entry.Name())[len(s.written):]
+						s.written = append(s.written, rest...)
+						index += ulen(rest)
 						break
 					}
 				}
@@ -248,8 +317,8 @@ func main() {
 			continue
 		default:
 			clear_written()
-			x++
-			s.written += key
+			s.written = append(s.written, []rune(key)...)
+			index++
 			draw_written()
 		}
 		c.Draw()
